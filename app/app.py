7480d79d9f4c6c48a8fa44b2cf0f8dacc7b91eb8
sahatsawat-pks <sahatsawat.nij@student.mahidol.ac.th>
2026-04-21 13:43:47 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -208,4 +208,4 @@ with gr.Blocks(css=CSS, title="🦴 Bone Fracture Detector") as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, show_error=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, show_error=True, share=True)
